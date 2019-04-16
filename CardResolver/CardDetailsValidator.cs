8c77c22a5f0132702523af62f31e958a4d3a1b66
aSidorok <a.sidorok@icloud.com>
2019-04-16 21:56:26 +0300

    add validator

---
+++ CardResolver/CardDetailsValidator.cs
@@ -0,0 +1,43 @@
+﻿namespace CardResolver
+{
+    public static class CardDetailsValidator
+    {
+        private const string VisaStart = "4";
+        private const string MasterCardStart = "5";
+        private const int MaxCardDigits = 16;
+        private const int MinVisaDigits = 13;
+
+        public static bool IsInvalidCardTypeRangeProvided(int cardType)
+        {
+            return cardType <= 0 || cardType >= 3;
+        }
+
+        public static string IsValidCardType(string cardNumber, int cardType)
+        {
+            var validationResult = cardType == 1
+                ? GetCardTypeValidationMessage(cardNumber, VisaStart)
+                : GetCardTypeValidationMessage(cardNumber, MasterCardStart);
+
+            return $"Card type provided is {validationResult}";
+        }
+
+        public static string IsValidCardTypeLength(string cardNumber, int cardType)
+        {
+            var isValidLength = cardType == 1
+                ? IsValidVisaCardNumberLength(cardNumber)
+                : cardNumber.Length == MaxCardDigits;
+
+            return $"Card length is {(isValidLength ? "valid" : "invalid")}";
+        }
+
+        private static string GetCardTypeValidationMessage(string cardNumber, string startWithDigit)
+        {
+            return cardNumber.StartsWith(startWithDigit) ? "valid" : "invalid";
+        }
+
+        private static bool IsValidVisaCardNumberLength(string cardNumber)
+        {
+            return cardNumber.Length >= MinVisaDigits && cardNumber.Length <= MaxCardDigits;
+        }
+    }
+}
--- CardResolver/Program.cs
+++ CardResolver/Program.cs
@@ -1,18 +1,17 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 
 namespace CardResolver
 {
     public class Program
     {
-        private const string VisaStart = "4";
-        private const string MasterCardStart = "5";
-
         static void Main()
         {
             Console.WriteLine("Please insert card number type: 1 for Visa, 2 for MasterCard");
             string cardTypeInput = Console.ReadLine();
 
-            if (!int.TryParse(cardTypeInput, out int cardType) || IsInvalidCardTypeRangeProvided(cardType))
+            if (!int.TryParse(cardTypeInput, out int cardType) || CardDetailsValidator.IsInvalidCardTypeRangeProvided(cardType))
             {
                 Console.WriteLine("Invalid card type");
                 Console.ReadKey();
@@ -23,7 +22,7 @@ namespace CardResolver
 
             string cardNumber = Console.ReadLine();
 
-            if (string.IsNullOrWhiteSpace(cardNumber))
+            if (string.IsNullOrWhiteSpace(cardNumber) || !cardNumber.All(char.IsDigit))
             {
                 Console.WriteLine("Your input string is incorrect");
                 Console.ReadKey();
@@ -31,25 +30,18 @@ namespace CardResolver
             }
 
             var luhnAlgorithm = new LuhnAlgorithm();
-            var result = luhnAlgorithm.IsCardValid(cardNumber) ? "valid" : "invalid";
-            var cardTypeValidation = IsValidCardType(cardNumber, cardType) ? "valid" : "invalid";
+            var cardNumberValidationResult = luhnAlgorithm.IsCardValid(cardNumber) ? "valid" : "invalid";
 
-            Console.WriteLine($"Card number is {result} and {cardTypeValidation} card type");
+            var messageResult = new StringBuilder();
 
+            messageResult.Append("Card number is ").AppendLine(cardNumberValidationResult);
+            messageResult.AppendLine(CardDetailsValidator.IsValidCardType(cardNumber, cardType));
+            messageResult.AppendLine(CardDetailsValidator.IsValidCardTypeLength(cardNumber, cardType));
+
+            Console.WriteLine("\nRESULTS:");
+            Console.WriteLine(messageResult.ToString());
 
             Console.ReadKey();
-        }
-
-        private static bool IsInvalidCardTypeRangeProvided(int cardType)
-        {
-            return cardType <= 0 || cardType >= 3;
-        }
-
-        private static bool IsValidCardType(string cardNumber, int cardType)
-        {
-            var startDigit = cardType == 1 ? VisaStart : MasterCardStart;
-
-            return cardNumber.StartsWith(startDigit);
         }
     }
 }
